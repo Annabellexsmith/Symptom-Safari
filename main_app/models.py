cc38874b13e62fd6e2b03dedf56155ea758ab3ae
AnnabellexSmith <sannabelle056@gmail.com>
2024-06-21 12:50:00 -0700

    note and symptom model added

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Symptom(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('symptom-detail', kwargs={'pk': self.id})
+
+class Note(models.Model):
+    user = models.ForeignKey(User,          on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    assessment = models.ManyToManyField(Symptom)
+    notes = models.TextField(max_length=500)
+    
+    def __str__(self):
+        return self.date.strftime("%b %d")
+    def get_absolute_url(self):
+        return reverse('note-detail', kwargs={'note_id': self.id})
+    
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="homepage"),
     path("about/", views.about, name="about"),
+    path("notes/", views.note_index, name="note-index"), 
 ]
