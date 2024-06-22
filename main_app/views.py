11b881012a906aa55da266881b62cbf74089be28
AnnabellexSmith <sannabelle056@gmail.com>
2024-06-22 09:20:47 -0700

    user auth implemented

--- main_app/views.py
+++ main_app/views.py
@@ -3,9 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from .models import Note, Symptom
-# from .forms import NoteForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DetailView 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -13,26 +11,36 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-
-def home(request):
-    return render(request, 'home.html')
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('note-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
 
 def about(request):
     return render(request, 'about.html')
-
+@login_required
 def note_index(request):    
     notes = Note.objects.filter(user=request.user)
     return render(request, 'notes/index.html', {
         'notes': notes, 
     })
-
+@login_required
 def note_detail(request, note_id):
     note = Note.objects.get(id=note_id)
     return render(request, 'notes/detail.html', {'note': note})
 
 
 
-class NoteCreate(CreateView):
+class NoteCreate(LoginRequiredMixin, CreateView):
     model = Note
     fields = ['assessment', 'notes']
     success_url = '/notes/'
@@ -40,10 +48,13 @@ class NoteCreate(CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-class NoteDelete(DeleteView):
+class NoteDelete(LoginRequiredMixin, DeleteView):
     model = Note
     success_url = '/notes/'
 
-class NoteUpdate(UpdateView):
+class NoteUpdate(LoginRequiredMixin, UpdateView):
     model = Note
     fields = ['assessment', 'notes']
+
+class Home(LoginView):
+    template_name = 'home.html'
