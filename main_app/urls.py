9a224c4e965180e73b79130245c7190f6fd62730
AnnabellexSmith <sannabelle056@gmail.com>
2024-06-22 06:52:06 -0700

    Full CRUD function for Note

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("", views.home, name="homepage"),
     path("about/", views.about, name="about"),
     path("notes/", views.note_index, name="note-index"), 
+    path("notes/<int:note_id>/", views.note_detail, name='note-detail'), 
+    path('notes/create/', views.NoteCreate.as_view(), name='note-create'),
+    path('notes/<int:pk>/update/', views.NoteUpdate.as_view(), name='note-update'),
+    path('notes/<int:pk>/delete/', views.NoteDelete.as_view(), name='note-delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from .models import Note, Symptom
+# from .forms import NoteForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView 
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('This will be the homepage')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
-def note_index(request):
-    return render(request, 'note_index.html' )
+def note_index(request):    
+    notes = Note.objects.filter(user=request.user)
+    return render(request, 'notes/index.html', {
+        'notes': notes, 
+    })
+
+def note_detail(request, note_id):
+    note = Note.objects.get(id=note_id)
+    return render(request, 'notes/detail.html', {'note': note})
+
+
+
+class NoteCreate(CreateView):
+    model = Note
+    fields = ['assessment', 'notes']
+    success_url = '/notes/'
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class NoteDelete(DeleteView):
+    model = Note
+    success_url = '/notes/'
+
+class NoteUpdate(UpdateView):
+    model = Note
+    fields = ['assessment', 'notes']
