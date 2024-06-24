06a7ef04430efb83aacd19c061d4991c7817b826
AnnabellexSmith <sannabelle056@gmail.com>
2024-06-24 07:41:51 -0700

    note model now shows last updated

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,17 +10,27 @@ class Symptom(models.Model):
 
     def __str__(self):
         return self.name
+    
     def get_absolute_url(self):
         return reverse('symptom-detail', kwargs={'pk': self.id})
 
 class Note(models.Model):
-    user = models.ForeignKey(User,          on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
     date = models.DateTimeField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
     assessment = models.ManyToManyField(Symptom)
     notes = models.TextField(max_length=500)
-    
+
     def __str__(self):
-        return self.date.strftime("%b %d")
+        # return self.date.strftime("%b %d")
+        return self.title
+    
     def get_absolute_url(self):
         return reverse('note-detail', kwargs={'note_id': self.id})
+    
+    def save (self, *args, **kwargs):
+        if not self.updated:
+            self.updated= timezone.now()
+        super().save(*args, **kwargs)
     
