47e3a48ea21aead8f8ac07d556c980a66d3ac69e
AnnabellexSmith <sannabelle056@gmail.com>
2024-06-27 06:33:04 -0700

    debugging config

--- symptomsafari/settings.py
+++ symptomsafari/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("pwW9B9LGTd")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if not 'ON_HEROKU' in os.environ:
